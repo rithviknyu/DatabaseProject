b97e2809a7610691e2ec5327b3e54ffddcc111ed
Rithvik Peeriga <peerigarithvik@gmail.com>
2023-12-02 11:27:32 -0500

    registration apis

---
+++ src/main/java/com/app/wow/car/rental/model/CorporateCustomer.java
@@ -0,0 +1,15 @@
+package com.app.wow.car.rental.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigInteger;
+
+@Entity(name = "PRS_CORPORATE2")
+@DiscriminatorValue("C")
+@Data
+public class CorporateCustomer extends Customer {
+    private String empid;
+    private BigInteger regisnum;
+
+}
---
+++ src/main/java/com/app/wow/car/rental/model/IndividualCustomer.java
@@ -0,0 +1,22 @@
+package com.app.wow.car.rental.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity(name = "PRS_INDIVIDUAL2")
+@DiscriminatorValue("I")
+@Data
+public class IndividualCustomer extends  Customer {
+
+    private  String fname;
+    private  String mname;
+    private String lname;
+    @Column(name = "dlicenseno")
+    private String drivingLicenseNumber;
+    @Column(name = "insname") private String insuranceName;
+    @Column(name = "inspnum") private String insuranceNumber;
+
+
+
+
+}
